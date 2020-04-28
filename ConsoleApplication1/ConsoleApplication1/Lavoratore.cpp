9da174439ecd44c41a5272946cc9da8568f3081c
thefumasgo <57558438+thefumasgo@users.noreply.github.com>
2020-04-28 18:12:12 +0200

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Lavoratore.cpp
@@ -0,0 +1,28 @@
+#include "Lavoratore.h"
+
+
+
+CLavoratore::CLavoratore()
+{
+}
+
+
+CLavoratore::~CLavoratore()
+{
+}
+
+void CLavoratore::modificaDisponibilità(CData data)
+{
+}
+
+void CLavoratore::aggiungiDisponibilità(CData data)
+{
+}
+
+void CLavoratore::annullaDisponibilità(CData data)
+{
+}
+
+void CLavoratore::visualizzaCalendario(CCalendario calendario)
+{
+}
