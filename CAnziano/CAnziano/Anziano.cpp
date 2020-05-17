e230770695de31ba0fb028581965fa77eb1a061b
lucaBallabio <57558348+lucaBallabio@users.noreply.github.com>
2020-05-17 19:52:57 +0200

    Add files via upload

--- CAnziano/CAnziano/Anziano.cpp
+++ CAnziano/CAnziano/Anziano.cpp
@@ -4,6 +4,68 @@
 
 CAnziano::CAnziano()
 {
+	indirizzo = "";
+	nome = "";
+	cognome = "";
+	lista = CLista();
+	nOrdine = 0;
+}
+
+CAnziano::CAnziano(string indirizzo, string nome, string cognome, CLista lista, int nOrdine)
+{
+	this->indirizzo = indirizzo;
+	this->nome = nome;
+	this->cognome = cognome;
+	this->lista = lista;
+	this->nOrdine = nOrdine;
+}
+
+
+void CAnziano::effettuaOrdine(string indirizzo, string nome, string cognome)
+{
+	ordini.addOrdine(indirizzo, nome, cognome);
+}
+
+void CAnziano::visualizzaOrdine(string cognome)
+{
+	int i = 0;
+	bool find = false;
+	while (find == false || i < ordini.length())
+	{
+		if (cognome == ordini[i].cognome)
+		{
+			ordini[i].visualizza;
+			find = true;
+		}
+	}
+}
+
+void CAnziano::modificaOrdine(string indirizzo, string nome, string cognome,CLista lista)
+{
+	int i = 0;
+	bool find = false;
+	while (find == false || i < ordini.length())
+	{
+		if (cognome == ordini[i].cognome)
+		{
+			ordini[i].modifica(indirizzo,nome,cognome,lista);
+			find = true;
+		}
+	}
+}
+
+void CAnziano::annullaOrdine(string cognome)
+{
+	int i = 0;
+	bool find = false;
+	while (find == false || i < ordini.length())
+	{
+		if (cognome == ordini[i].cognome)
+		{
+			ordini[i].cancellaOrdine();
+			find = true;
+		}
+	}
 }
 
 
--- CAnziano/CAnziano/Anziano.h
+++ CAnziano/CAnziano/Anziano.h
@@ -9,13 +9,14 @@ class CAnziano
 private:
 	string indirizzo, nome, cognome;
 	CLista lista;
+	int nOrdine;
 public:
 	//il nome per identificare l'ordine sarà il cognome dell'utante
 	CAnziano();
-	CAnziano(string indirizzo,string nome,string cognome,CLista lista);
-	void effettuaOrdine(CLista lista);
+	CAnziano(string indirizzo,string nome,string cognome,CLista lista,int nOrdine);
+	void effettuaOrdine(string indirizzo, string nome, string cognome);
 	void visualizzaOrdine(string cognome);
-	void modificaOrdine(string cognome);
+	void modificaOrdine(string indirizzo, string nome, string cognome, CLista lista);
 	void annullaOrdine(string cognome);
 	~CAnziano();
 };
